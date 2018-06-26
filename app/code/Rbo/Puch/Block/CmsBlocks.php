8861a5e3aff5cc0620498b781fd5fc07fdb61b77
tspecialist <tvorexspecialist@gmail.com>
2018-06-26 23:04:39 +0800

    get m1 footer and styles

--- app/code/Rbo/Puch/Block/CmsBlocks.php
+++ app/code/Rbo/Puch/Block/CmsBlocks.php
@@ -6,16 +6,27 @@ use Magento\Cms\Api\BlockRepositoryInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Catalog\Helper\Category;
+use Magento\Store\Model\StoreManagerInterface;
 
 class CmsBlocks extends Template
 {
     private $blockRepository;
+
+    private $categoryHelper;
+
+    private $storeManager;
+
     public function __construct(
         BlockRepositoryInterface $blockRepository,
         Context $context,
+        Category $categoryHelper,
+        StoreManagerInterface $storeManager,
         array $data = []
     ) {
         $this->blockRepository = $blockRepository;
+        $this->categoryHelper = $categoryHelper;
+        $this->storeManager = $storeManager;
         parent::__construct($context, $data);
     }
 
@@ -29,5 +40,18 @@ class CmsBlocks extends Template
         }
 
         return $content;
+    }
+
+    public function getCategories(){
+        return $this->categoryHelper->getStoreCategories();
+    }
+    public function getCategoryUrl($categoryId){
+        return $this->categoryHelper->getCategoryUrl($categoryId);
+    }
+
+    public function getLogoUrl($filename){
+        $mediaUrl = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+        $logoUrl = $mediaUrl . DIRECTORY_SEPARATOR . 'logo' . DIRECTORY_SEPARATOR . $filename;
+        return $logoUrl;
     }
 }
